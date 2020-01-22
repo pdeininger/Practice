06fb2bc8b8d054b713ebe4945ac76c4064d44d5a
Pam Deininger <pameladeininger@gmail.com>
2020-01-21 20:12:47 -0500

    updated link

--- Assets/javascript/search.js
+++ Assets/javascript/search.js
@@ -24,26 +24,29 @@ searchButton.on("click", function() {
   currentWeather(city);
   forecastWeather(city);
 });
+//create a function so if no search history, page still launches.  Only pulls from local storage if there is history.
 
 //create a function that takes search history from local storage and creates search history buttons as soon as the page loads
 function historyButtons() {
   //get our array from local storage
   searchHistoryArray = JSON.parse(localStorage.getItem("searchHistory"));
-  console.log(searchHistoryArray);
-  //need to have the default return reflecting if there is no storage so if there is, only then will it try to pull anything
-  if (searchHistoryArray.length === null || searchHistoryArray.length < 1) {
+  // console.log(searchHistoryArray);
+
+  if (searchHistoryArray === null || searchHistoryArray.length < 1) {
+    searchHistoryArray = [];
     return;
-  }
-  //for loop to create buttons from the array
-  for (var i = searchHistoryArray.length - 1; i >= 0; i--) {
-    console.log("inside the for loop");
-    // create a button for this index of the array
-    var newButton = $("<button>");
-    newButton.text(searchHistoryArray[i]);
-    newButton.addClass("search-history-button");
-    //create a click event for new button
-    //put the button inside the search history div
-    searchHistoryDiv.append(newButton);
+  } else {
+    //for loop to create buttons from the array
+    for (var i = searchHistoryArray.length - 1; i >= 0; i--) {
+      console.log("inside the for loop");
+      // create a button for this index of the array
+      var newButton = $("<button>");
+      newButton.text(searchHistoryArray[i]);
+      newButton.addClass("search-history-button");
+      //create a click event for new button
+      //put the button inside the search history div
+      searchHistoryDiv.append(newButton);
+    }
   }
 }
 
